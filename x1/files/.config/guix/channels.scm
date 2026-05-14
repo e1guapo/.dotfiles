42820708f00088eb9479ceadc8ba9ca909accba1
Dennis Losa <dennis.losa@gmail.com>
2026-05-14 09:58:00 -0400

    x1: updates

--- x1/files/.config/guix/channels.scm
+++ x1/files/.config/guix/channels.scm
@@ -1,33 +1,30 @@
 (list (channel
-        (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix")
-        (branch "master")
-        (commit
-          "880afd230881738b02498d6fbfae0529ae93d036")
-        (introduction
-          (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit "5f2630e69fbbe9e79c350a67545f0fef7e93e223")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-        (name 'guix-zephyr)
-        (url "https://github.com/paperclip4465/guix-zephyr")
-        (branch "master")
-        (commit
-          "c42d07cf11652045ce7cf3979053846ad789622f")
-        (introduction
-          (make-channel-introduction
-            "a0146bb3d13920074d40ed6d6d53321b17e267fb"
-            (openpgp-fingerprint
-              "089F 0E56 0087 9656 C463  691A 3811 C00D C428 5E27"))))
+       (name 'guix-zephyr)
+       (url "https://github.com/paperclip4465/guix-zephyr")
+       (branch "master")
+       (commit "67075e608cd7148711868228d0140f27a2bc3f54")
+       (introduction
+        (make-channel-introduction
+         "a0146bb3d13920074d40ed6d6d53321b17e267fb"
+         (openpgp-fingerprint
+          "089F 0E56 0087 9656 C463  691A 3811 C00D C428 5E27"))))
       (channel
-        (name 'guix)
-        (url "https://git.guix.gnu.org/guix.git")
-        (branch "master")
-        (commit
-          "b104a808ca028617d530c4b263c9bc419dd502cb")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+       (name 'guix)
+       (url "https://git.guix.gnu.org/guix.git")
+       (branch "master")
+       (commit "370d7883cfb2ae1ce0628e16d9c9d2fc4e8280ec")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
